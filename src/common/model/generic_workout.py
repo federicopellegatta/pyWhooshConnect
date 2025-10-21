b28623ba01dbf4863d6001eb28c0c9c0daa06d1f
Federico Pellegatta <pellegatta.federico@gmail.com>
2025-10-21 10:07:50 +0200

    refactor: make duration_in_seconds an abstract property in GenericWorkoutStep

--- src/common/model/generic_workout.py
+++ src/common/model/generic_workout.py
@@ -8,13 +8,16 @@ from typing import List, Optional
 
 class GenericWorkoutStep(ABC):
     step_id: int
-    duration_in_seconds: int
     type: str
     description: Optional[str]
 
+    @property
     @abstractmethod
-    def duration(self) -> timedelta:
+    def duration_in_seconds(self) -> int:
         ...
+
+    def duration(self) -> timedelta:
+        return timedelta(seconds=self.duration_in_seconds)
 
 
 @dataclass
@@ -26,22 +29,19 @@ class GenericAtomicStep(GenericWorkoutStep):
     description: Optional[str] = None
     rpm: Optional[int] = None
 
-    def duration(self) -> timedelta:
-        return timedelta(seconds=self.duration_in_seconds)
-
 
 @dataclass
 class GenericStepWithIntervals(GenericWorkoutStep):
     step_id: int
-    duration_in_seconds: int
     type: str
     intervals: List[GenericAtomicStep]
     iterations: int
     description: Optional[str] = None
 
-    def duration(self) -> timedelta:
+    @property
+    def duration_in_seconds(self) -> int:
         total = sum(i.duration_in_seconds for i in self.intervals or [])
-        return timedelta(seconds=total * (self.iterations or 1))
+        return total * (self.iterations or 1)
 
 
 @dataclass
