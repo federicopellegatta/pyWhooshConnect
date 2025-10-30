b5aedab716c570917595b5914dc054069be6c30d
PELLEGATTA FEDERICO <federico.pellegatta@intesasanpaolo.com>
2025-10-30 11:59:56 +0100

    refactor: split sync and download logic into two separate methods

--- src/service/workout_sync_service.py
+++ src/service/workout_sync_service.py
@@ -1,7 +1,7 @@
 import json
 from datetime import datetime, timedelta
 from pathlib import Path
-from typing import Optional
+from typing import Optional, List
 
 from src.common.mapper.base import PowerZonesOptions
 from src.garmin.client.GarminClient import GarminClient
@@ -10,6 +10,7 @@ from src.garmin.mapper.garmin_to_generic_workout import GarminToGenericScheduled
 from src.garmin.model.garmin_workout_dto import GarminSport
 from src.garmin.service.garmin_training_plan_service import GarminTrainingPlanService
 from src.mywhoosh.mapper.generic_to_mywhoosh import GenericToMyWhooshWorkoutMapper
+from src.mywhoosh.model.mywhoosh_workout_dto import MyWhooshWorkout
 
 
 class GarminToMyWhooshWorkoutSyncService:
@@ -31,8 +32,7 @@ class GarminToMyWhooshWorkoutSyncService:
             sport: GarminSport,
             from_date: datetime = datetime.today(),
             to_date: Optional[datetime] = None,
-            output_dir: str = "~/downloads/",
-    ):
+    ) -> List[MyWhooshWorkout]:
         # Retrieve Garmin workouts
         to_date = to_date if to_date is not None else (from_date + timedelta(days=7))
         garmin_workouts = self.garmin_training_plan_service.get_scheduled_workouts(
@@ -46,11 +46,26 @@ class GarminToMyWhooshWorkoutSyncService:
         power_zones = GarminToGenericPowerZonesMapper().map(garmin_power_zones)
         power_zones_options = PowerZonesOptions(power_zones=power_zones)
 
-        # Map each Garmin workout to MyWhoosh format and save it
+        # Map each Garmin workout to MyWhoosh format and return it
+        mywhoosh_workouts = []
         for garmin_workout in garmin_workouts:
             generic_workout = GarminToGenericScheduledWorkoutMapper().map(garmin_workout)
             mywhoosh_workout = GenericToMyWhooshWorkoutMapper().map(generic_workout, power_zones_options)
+            mywhoosh_workouts.append(mywhoosh_workout)
 
+        return mywhoosh_workouts
+
+    def sync_and_download_workouts(
+            self,
+            sport: GarminSport,
+            from_date: datetime = datetime.today(),
+            to_date: Optional[datetime] = None,
+            output_dir: str = "~/downloads/",
+    ):
+        mywhoosh_workouts = self.sync_workouts(sport, from_date, to_date)
+
+        # Map each Garmin workout to MyWhoosh format and save it
+        for mywhoosh_workout in mywhoosh_workouts:
             data = json.loads(mywhoosh_workout.to_json())
 
             output_dir = Path(output_dir).expanduser()
