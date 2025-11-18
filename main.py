a7c36876297c22426244f05aa0f67371f275569d
Federico Pellegatta <pellegatta.federico@gmail.com>
2025-11-18 16:08:09 +0100

    feat: add command line params using argparse

--- main.py
+++ main.py
@@ -1,13 +1,86 @@
+import argparse
+from datetime import datetime
+from typing import Optional
+
 from src import __title__, __version__, __description__
+from src.garmin.model.garmin_workout_dto import GarminSport
+
+def run_sync_logic(user: Optional[str], password: Optional[str], sport: Optional[str],
+                   from_date: Optional[str], to_date: Optional[str]):
+    """
+    Main function containing the application's synchronization and integration logic.
+    """
+    try:
+        start_date = datetime.strptime(from_date, '%Y-%m-%d').date() if from_date else None
+        end_date = datetime.strptime(to_date, '%Y-%m-%d').date() if to_date else None
+    except ValueError:
+        print("Error: Date format must be YYYY-MM-DD.")
+        return
+
+    try:
+        sport = GarminSport[sport.upper()] if sport else GarminSport.CYCLING
+    except KeyError:
+        print(
+            f"Error: sport not recognized. Valid options are: {', '.join([s.name for s in GarminSport])}.")
+        return
+
+    # TODO If no user/password, attempt to load from environment variables
+    # or fail the process later in your application logic.
+
+    print("--- Application Finished ---")
+
 
 def main():
     width: int = 50
 
-    print("="*width)
+    print("=" * width)
     print(f"{__title__} v{__version__}".center(width))
     print(f"{__description__}".center(width))
-    print("="*width)
+    print("=" * width)
     print("\n✓ Application started successfully!")
+
+    parser = argparse.ArgumentParser(
+        description="Synchronize workout data between services.",
+        epilog="Example: python main.py --user=john.doe --from_date=2025-01-01"
+    )
+
+    # All arguments are defined with default=None, making them optional.
+
+    parser.add_argument(
+        '--user',
+        type=str,
+        default=None,
+        help='Service username (optional).'
+    )
+    parser.add_argument(
+        '--password',
+        type=str,
+        default=None,
+        help='Service password (optional).'
+    )
+    parser.add_argument(
+        '--sport',
+        type=str,
+        default="cycling",
+        help='Filter by sport type (e.g., cycling, running, cross_country_skiing). Optional.'
+    )
+    parser.add_argument(
+        '--from_date',
+        type=str,
+        default=None,
+        help='Start date of the synchronization range [YYYY-MM-DD] (optional).'
+    )
+    parser.add_argument(
+        '--to_date',
+        type=str,
+        default=None,
+        help='End date of the synchronization range [YYYY-MM-DD] (optional).'
+    )
+
+    args = parser.parse_args()
+
+    # Call the core application logic
+    run_sync_logic(args.user, args.password, args.sport, args.from_date, args.to_date)
 
 
 if __name__ == '__main__':
